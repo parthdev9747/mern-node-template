1aaaea1523b379bc1e62aab8d5dd764c5c8755ca
parthdev <67677747+parthdev9747@users.noreply.github.com>
2024-09-20 11:55:21 +0530

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+import { config as conf } from "dotenv";
+conf()
+
+const config = {
+    port: process.env.PORT
+}
+
+export default config;
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import config from "./config/config"
+
+console.log("Port No: "+config.port);
